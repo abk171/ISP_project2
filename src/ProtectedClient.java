defe7aeb2154ecf7d5c012dd80f591d45eec7e5f
ak1909552 <82260503+ak1909552@users.noreply.github.com>
2025-03-02 20:05:04 -0500

    completed RSA client and server

--- src/ProtectedClient.java
+++ src/ProtectedClient.java
@@ -1,6 +1,10 @@
 import java.io.*;
 import java.net.*;
 import java.security.*;
+import java.security.spec.EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+import javax.crypto.*;
 import java.util.Date;
 
 public class ProtectedClient
@@ -10,15 +14,31 @@ public class ProtectedClient
 		DataOutputStream out = new DataOutputStream(outStream);
 
 		// IMPLEMENT THIS FUNCTION.
-		
+		try (FileInputStream keyIn = new FileInputStream("serverPublicKey.bin");){
+			byte[] serverPublicKeyBytes = keyIn.readAllBytes();
+			
 
+			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+			EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(serverPublicKeyBytes);
+			PublicKey serverPublicKey = keyFactory.generatePublic(publicKeySpec);
 
-		out.flush();
+			String userPassPair = user + ":" + password;
+
+			Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+			cipher.init(Cipher.ENCRYPT_MODE, serverPublicKey);
+			byte[] encryptedUserPassPair = cipher.doFinal(userPassPair.getBytes());
+			
+			out.write(encryptedUserPassPair);
+
+			out.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) throws Exception 
 	{
-		String host = "your localhost";
+		String host = "127.0.0.1";
 		int port = 7999;
 		String user = "George";
 		String password = "abc123";
--- src/ProtectedServer.java
+++ src/ProtectedServer.java
@@ -1,25 +1,61 @@
 import java.io.*;
 import java.net.*;
 import java.security.*;
+import javax.crypto.*;
 
 public class ProtectedServer
 {
+	private PrivateKey privateKey;
+	public ProtectedServer() {
+		try(FileOutputStream fos = new FileOutputStream("serverPublicKey.bin");) {
+			KeyPairGenerator keygen = KeyPairGenerator.getInstance("RSA");
+			keygen.initialize(2048);
+			KeyPair pair = keygen.generateKeyPair();
+
+			PublicKey pkey = pair.getPublic();
+			this.privateKey = pair.getPrivate();
+
+			fos.write(pkey.getEncoded());
+			System.out.println("Generated key pair");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	public boolean authenticate(InputStream inStream) throws IOException, NoSuchAlgorithmException 
 	{
-		DataInputStream in = new DataInputStream(inStream);
+		try {
+			DataInputStream in = new DataInputStream(inStream);
+			Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+			cipher.init(Cipher.DECRYPT_MODE, this.privateKey);
+			byte[] encryptedUserPassPair = in.readAllBytes();
+			byte[] decryptedUserPassPair = cipher.doFinal(encryptedUserPassPair);
+			
+			String userPassPair = new String(decryptedUserPassPair);
+			String[] parts = userPassPair.split(":", 2);
 
-		// IMPLEMENT THIS FUNCTION.
+			if (parts[1].equals(lookupPassword(parts[0])))
+				return true;
+			else
+				return false;
+			// IMPLEMENT THIS FUNCTION.
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+
+	
+		
 	}
 
 	protected String lookupPassword(String user) { return "abc123"; }
 
 	public static void main(String[] args) throws Exception 
 	{
+		ProtectedServer server = new ProtectedServer();
 		int port = 7999;
 		ServerSocket s = new ServerSocket(port);
 		Socket client = s.accept();
-
-		ProtectedServer server = new ProtectedServer();
 
 		if (server.authenticate(client.getInputStream()))
 		  System.out.println("Client logged in.");
